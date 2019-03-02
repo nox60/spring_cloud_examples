bd32a980489714a7ecf8d97c0999d290e52d2c5e
Liu Li <nox60@hotmail.com>
2019-03-02 23:52:29 +0800

    Update EurekaApp.java

--- without-lb/eureka/src/main/java/com/nox/scloud/eureka/EurekaApp.java
+++ without-lb/eureka/src/main/java/com/nox/scloud/eureka/EurekaApp.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApp {
     public static void main(String[] args) {
-    
+ 
         new SpringApplicationBuilder(EurekaApp.class).web(true).run(args);
     }
 }
